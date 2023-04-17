8cc7a79825487016330ad495c4e6adc74a11119b
yqy <1071665725@qq.com>
2023-04-17 18:08:20 +0800

    增加几个脚本,调参和多线程跑结果

---
+++ code/pipe.py
@@ -0,0 +1,40 @@
+import os
+import datetime
+import subprocess
+import random
+import re
+from multiprocessing import Process, Manager
+
+
+def runMapBlue(mapName, randomSeed):
+    com = f'../judge-1/Robot -s {randomSeed} -m ../judge-1/maps/{mapName}.txt -r ../judge-1/replay/{mapName}-Blue-{randomSeed}.rep -f ./main ../judge-1/Demo/SimpleDemo'
+    output = subprocess.run(com, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    match = re.search(r'"score":\[(\d+),(\d+)\]', output.stdout.decode('utf-8'))
+    return match.group(1)
+def runMapRed(mapName, randomSeed):
+    com = f'../judge-1/Robot -s {randomSeed} -m ../judge-1/maps/{mapName}.txt -r ../judge-1/replay/{mapName}-Red-{randomSeed}.rep -f ../judge-1/Demo/SimpleDemo ./main'
+    output = subprocess.run(com, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    match = re.search(r'"score":\[(\d+),(\d+)\]', output.stdout.decode('utf-8'))
+    return match.group(2)
+
+if os.system('g++ main.cpp -o main -std=c++17 -DEBUG -DCREATE -O3 -pthread') != 0:
+    print("compiling failed")
+    exit(0)
+print("compiling successfully")
+
+def run():
+    randomSeed = random.randint(0, 65535)
+    mapList = ["1", "2"]
+    for i, mapName in enumerate(mapList):
+        scoreBlue = runMapBlue(mapName, randomSeed)
+        scoreRed = runMapRed(mapName, randomSeed)
+        print("map=", mapName, "seed=", randomSeed, "scoreBlue=", scoreBlue, "scoreRed=", scoreRed)
+processes = []
+for step in range(16):
+    print("step = ", step)
+    for threadNum in range(32):
+        p = Process(target=run)
+        processes.append(p)
+        p.start()
+    for p in processes:
+        p.join()
--- code/tc.py
+++ code/tc.py
@@ -2,9 +2,11 @@ import os
 import datetime
 import subprocess
 import random
-from tqdm import trange
+from multiprocessing import Process, Manager
+# from tqdm import trange
+ans_path = '../log/ans.csv'
 
-def runMap(mapName, randomSeed):
+def runMap(mapName, randomSeed,sharedList):
     com = f'../judge-1/Robot -s {randomSeed} -m ../judge-1/maps/{mapName}.txt -r ../judge-1/replay/{mapName}-{datetime.datetime.now().strftime("%Y-%m-%d.%H.%M.%S")}.rep -f ./main'
     # print(com)
     output = subprocess.run(com, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -12,12 +14,14 @@ def runMap(mapName, randomSeed):
     # input()
     # statu = output.stdout.decode('utf-8').split('"status":')[1].split(',')[0]
     score = output.stdout.decode('utf-8').split('"score":')[1].split('}')[0]
-    return score
+    # return score
+    # sharedList.
+    sharedList.append(int(score))
 
-os.system('rm /q "../judge-1/replay/*"')
-if os.system('g++ main.cpp -o main -std=c++17 -DEBUG -O3 -pthread') != 0:
-    print("compiling failed")
-    exit(0)
+# os.system('rm /q "../judge-1/replay/*"')
+# if os.system('g++ main.cpp -o main -std=c++17 -DEBUG -O3 -pthread') != 0:
+#     print("compiling failed")
+#     exit(0)
     '''
     for mapNum in range(1, 5):
         print("running on:", mapNum)
@@ -34,17 +38,17 @@ if os.system('g++ main.cpp -o main -std=c++17 -DEBUG -O3 -pthread') != 0:
     if os.path.isdir("replay"):
         os.system('rd /s /q "replay"')
     '''
-print("compiling successfully")
+# print("compiling successfully")
 
 def writeConfg(cfgList):
     with open("tc.txt", "w") as f:
         for cfg in cfgList:
             f.write(str(cfg) + "\n")
 
-def run(stepNumber=16, randomNumber=8):
-    mapList = ["1", "2"]
-    beginValue = 0.2
-    endValue = 2.0
+def run(stepNumber=8, randomNumber=32):
+    mapList = ["2"]
+    beginValue = 1
+    endValue = 9
     interval = (endValue - beginValue) / stepNumber
     nowValue = beginValue
     for step in range(stepNumber):
@@ -53,14 +57,29 @@ def run(stepNumber=16, randomNumber=8):
         nowValue += interval
         writeConfg(cfgList)
         for i, mapName in enumerate(mapList):
-            mapScore = []
-            for j in trange(randomNumber):
+            manager = Manager()
+            mapScore = manager.list()
+            processes = []
+            for j in range(randomNumber):
                 randomSeed = random.randint(0, 65535)
-                score = runMap(mapName, randomSeed)
-                mapScore.append(int(score))
-            print("nowMap:", mapName, "nowValue:", cfgList[0])
-            print("\tmapScoresAcg:", sum(mapScore) / len(mapScore))
-            print("\tmapScoresMin:", min(mapScore))
-            print("\tmapScoresMax:", max(mapScore))
+                p = Process(target=runMap, args=(mapName, randomSeed, mapScore))
+                # score = runMap(mapName, randomSeed)
+                processes.append(p)
+                p.start()
+            for p in processes:
+                p.join()
+            mapScore = list(mapScore)
+            with open(ans_path,'a') as ans_file:
+                print("nowMap:", mapName, "nowValue:", cfgList[0],file=ans_file)
+                print("\tmapScoresAcg:", sum(mapScore) / len(mapScore),file=ans_file)
+                print("\tmapScoresMin:", min(mapScore),file=ans_file)
+                print("\tmapScoresMax:", max(mapScore),file=ans_file)
 
-run()
+if __name__ == "__main__":
+    open(ans_path,'w').close()
+    os.system('rm /q "../judge-1/replay/*"')
+    if os.system('g++ main.cpp -o main -std=c++17 -DEBUG -O3 -pthread') != 0:
+        print("compiling failed")
+        exit(0)
+    print("compiling successfully")
+    run()
